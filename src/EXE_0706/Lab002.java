a88bcc0c47ab60d1f1103016ae8c96b3c54a724c
1403vasu <vasumathisrithar@gmail.com>
2024-07-30 13:05:57 +0530

    second exe

---
+++ src/EXE_0706/Lab002.java
@@ -0,0 +1,11 @@
+package EXE_0706;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = a+b;
+        System.out.println(c);
+    }
+
+}
